ea4aa3280fa9a823b2cfe443347ee4b756d2179e
Paola187 <84358958+Paola187@users.noreply.github.com>
2021-05-17 20:04:34 -0500

    Add files via upload

---
+++ FUNCION_PRIMITIVA_PAOLA_MONTES_3202.m
@@ -0,0 +1,17 @@
+clc
+clear
+l= (+3:-2);
+d= (5:+5);
+l= (+2:-2);
+m= (-2:+2);
+hold on
+plot (l, 'r','linewidth',7)
+
+plot (d, 'r','linewidth',4)
+
+plot (l, 'r','linewidth',2)
+ 
+ plot (m, 'r','linewidth',9)
+
+
+hold off
