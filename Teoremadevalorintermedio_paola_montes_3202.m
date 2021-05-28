c66584d69b43f4436f3a204bd38efaa4c1886f47
Paola187 <84358958+Paola187@users.noreply.github.com>
2021-05-28 17:27:30 -0500

    Add files via upload

---
+++ Teoremadevalorintermedio_paola_montes_3202.m
@@ -0,0 +1,18 @@
+clc
+clear
+a= (-1:+4);
+b= (-2:+2);
+c= (+2:-4);
+d= (-2:-5);
+hold on
+plot (a, 'r','linewidth', 2)
+
+plot (b, 'r','linewidth' ,6)
+
+plot (c, 'r','linewidth' ,9)
+
+plot (d, 'r','linewidth' ,7 )
+
+
+
+hold off
