0c7ff7fcbf7f16ed86f902411c6a75d46aa20be8
Paola187 <84358958+Paola187@users.noreply.github.com>
2021-05-28 19:04:22 -0500

    Add files via upload

--- Teoremafundamentaldelcalculo_Paola_montes_3202.m
+++ Teoremafundamentaldelcalculo_Paola_montes_3202.m
@@ -1,18 +1,64 @@
-clc
-clear
-a= (-1:+4);
-b= (-2:+2);
-c= (+2:-4);
-d= (-2:-5);
-hold on
-plot (a, 'r','linewidth', 2)
+figure;
+x = 0:.1:4*pi;
 
-plot (b, 'r','linewidth' ,6)
+y1 = sin(x);
+y2 = cos(x);
+y3 = 2*cos(x);
 
-plot (c, 'r','linewidth' ,9)
+hold on;
 
-plot (d, 'r','linewidth' ,7 )
+plot(x,y3);
+plot(x,y2);
 
+hold off;
 
+figure;
+plot(x,y3);
+figure;
+x = 0:.6:4*pi;
 
-hold off
+y1 = sin(x);
+y2 = cos(x);
+y3 = 2*cos(x);
+
+hold on;
+
+plot(x,y1);
+plot(x,y3);
+
+hold off;
+
+figure;
+plot(x,y3);
+figure;
+x = 0:.4:7*pi;
+
+y1 = sin(x);
+y2 = cos(x);
+y3 = 2*cos(x);
+
+hold on;
+
+plot(x,y1);
+plot(x,y3);
+
+hold off;
+
+figure;
+plot(x,y3); 
+figure;
+x = 0:.3:1*pi;
+
+y1 = sin(x);
+y2 = cos(x);
+y3 = 2*cos(x);
+
+hold on;
+
+plot(x,y3);
+plot(x,y2);
+
+hold off;
+
+figure;
+plot(x,y3); 
